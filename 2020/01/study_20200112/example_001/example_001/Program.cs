5b07683a0f6f8a8039344df3c5f4e5c1fc1bd712
Taker829 <kt829@naver.com>
2020-01-12 18:07:47 +0900

    no message

---
+++ 2020/01/study_20200112/example_001/example_001/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+
+namespace IncDecOperator
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            WriteLine(a++);
+            WriteLine(++a);
+
+            WriteLine(a--);
+            WriteLine(--a);
+        }
+    }
+}
