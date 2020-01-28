72499868f78f88ad68adddf9a429e2606287ef1c
Taker829 <kt829@naver.com>
2020-01-28 13:54:58 +0900

    C# study 4강까지 진행, 2020-01-28

---
+++ 2020/01/study_20200128/example_001/example_001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace example_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ?? 좌측의 비교값이 null이 맞다면 우측의 출력 실행
+            int? num = null;
+            WriteLine($"{num ?? 0}");
+
+            num = 99;
+            WriteLine($"{num ?? 0}");
+
+            string str = null;
+            WriteLine($"{str ?? "Default"}");
+
+            str = "Specific";
+            WriteLine($"{str ?? "Default"}");
+        }
+    }
+}
