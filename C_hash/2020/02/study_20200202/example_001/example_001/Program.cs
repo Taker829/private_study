b952af138d13d5d756eb5df77603fc406a5abd3b
Taker829 <kt829@naver.com>
2020-02-02 17:35:04 +0900

    Python 추가 및 폴더 구조 변경

---
+++ C_hash/2020/02/study_20200202/example_001/example_001/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace example_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요 : ");
+
+            // Console.ReadLine()은 사용자로부터 문자열을 입력받아 그 결과를 반환하는 기능.
+            //string input = Console.ReadLine();
+            string input = ReadLine();
+            int number = Int32.Parse(input);
+
+            if (number < 0)
+                WriteLine("음수");
+            else if (number > 0)
+                WriteLine("양수");
+            else
+                WriteLine("0");
+
+            if (number % 2 == 0)
+                WriteLine("짝수");
+            else
+                WriteLine("홀수");
+        }
+    }
+}
