647f1e917a7ce6b6d64b272361fdb71679f4335d
Taker829 <kt829@naver.com>
2020-02-07 18:25:02 +0900

    2020-02-07 C# study update
    while & do while

---
+++ C_hash/2020/02/study_20200207/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            while(i>0)
+            {
+                //지속적으로 감소
+                WriteLine($"i : {i--}");
+            }
+        }
+    }
+}
---
+++ C_hash/2020/02/study_20200207/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+
+namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+
+            do
+            {
+                WriteLine("a) i : {0}", i--);
+            }
+            while (i > 0);
+
+            do
+            {
+                WriteLine("b) i : {0}", i--);
+            }
+            while (i > 0);
+        }
+    }
+}
