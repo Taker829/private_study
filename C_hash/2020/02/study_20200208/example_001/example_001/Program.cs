76889e1fb8cc49c3ffe1874c67a1ca45d6152b2d
Taker829 <kt829@naver.com>
2020-02-08 15:15:53 +0900

    2020-02-08 C# update
    for 문 학습 (중첩 for 문)

---
+++ C_hash/2020/02/study_20200208/example_001/example_001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace example_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                WriteLine(i);
+            }
+        }
+    }
+}
---
+++ C_hash/2020/02/study_20200208/example_002/example_002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace example_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i < 5; i++)
+            {
+                for (int j=0; j <= i; j++)
+                {
+                    Write("*");
+                }
+                WriteLine();
+            }
+        }
+    }
+}
