836296a5a91f2efb456b8b7d11338d13fc3b2187
Taker829 <kt829@naver.com>
2020-02-18 16:55:03 +0900

    2020-02-18 C# study update
    goto 까지 공부했음

---
+++ C_sharp/2020/02/study_20200218/example_001/example_001/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace example_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int loop_cnt = 0;
+
+            while (true)
+            {
+                Write("계속할까요? (예/아니오) : ");
+                string answer = ReadLine();
+
+                if (answer == "아니오")
+                    break;
+                else if (answer == "예")
+                {
+                    loop_cnt++;
+                    WriteLine($"반복 횟수 : {loop_cnt}");
+                }
+                else
+                    WriteLine("입력 오류, 재입력하세요");
+            }
+        }
+    }
+}
