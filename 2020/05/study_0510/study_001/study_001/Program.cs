424e42a3a0f8b9156b76b4a13ef34f8760d6481c
Taker829 <kt829@naver.com>
2020-05-10 21:58:55 +0900

    2020-05-10 study update
    method overloading

---
+++ 2020/05/study_0510/study_001/study_001/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace Overloading
+{
+    class MainApp
+    {
+        static int Plus (int a, int b)
+        {
+            WriteLine("Calling int Plus(int, int)...");
+            return a + b;
+        }
+
+        static int Plus (int a, int b, int c)
+        {
+            WriteLine("Calling int Plus(int, int, int)...");
+            return a + b + c;
+        }
+
+        static double Plus (double a, double b)
+        {
+            WriteLine("Calling double Plus(double, double)...");
+            return a + b;
+        }
+
+        static double Plus (double a, double b, double c)
+        {
+            WriteLine("Calling double Plus (double, double, double)...");
+            return a + b + c;
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLine(Plus(1, 2));
+            WriteLine(Plus(1, 2, 3));
+            WriteLine(Plus(1.2, 2.4));
+            WriteLine(Plus(1.2, 2.4, 3.6));
+        }
+    }
+}
