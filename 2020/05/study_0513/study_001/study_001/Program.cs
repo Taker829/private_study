d9f1c1fe67f3fd0f7d1729c1c7d62785719acd38
Taker829 <kt829@naver.com>
2020-05-13 08:21:49 +0900

    2020-05-13 private study update
    C# OptionalParameter study update

---
+++ 2020/05/study_0513/study_001/study_001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace study_001
+{
+    class Program
+    {
+        static void PrintProfile(string name, string phone = "")
+        {
+            WriteLine($"Name:{name}, Phone:{phone}");
+        }
+        static void Main(string[] args)
+        {
+            PrintProfile("태연");
+            PrintProfile("윤아", "010-1234-1234");
+            PrintProfile(name : "유리");
+            PrintProfile(name: "서현", phone: "010-7894-7895");
+        }
+    }
+}
