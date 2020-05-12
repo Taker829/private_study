ebeba81d276bb6428109994fce43a8b6ba0b0e24
Taker829 <kt829@naver.com>
2020-05-12 18:00:39 +0900

    2020-05-12 study update
    C# NamedParameter study update

---
+++ 2020/05/study_0511/study_001/study_001/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace study_001
+{
+    class Program
+    {
+        static int Sum(params int[] args)
+        {
+            WriteLine("Sum nums.......");
+            int sum = 0;
+            for(int i = 0; i<args.Length; i++)
+            {
+                if (i > 0)
+                    Write(", ");
+
+                Write(args[i]);
+                sum += args[i];
+            }
+            WriteLine();
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            int sum = Sum(3, 4, 5, 6, 7, 8, 9, 10);
+            WriteLine($"Sum : {sum}");
+        }
+    }
+}
---
+++ 2020/05/study_0512/study_001/study_001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+namespace study_001
+{
+    class Program
+    {
+        static void PrintProfile(string name, string phone)
+        {
+            WriteLine($"Name:{name}, Phone:{phone}");
+        }
+
+        static void Main(string[] args)
+        {
+            PrintProfile("박찬호", "010-1234-5959");
+            PrintProfile(name:"박지성", phone:"010-4234-6512");
+            PrintProfile(phone: "010-7894-5123", name: "김경태");
+        }
+    }
+}
