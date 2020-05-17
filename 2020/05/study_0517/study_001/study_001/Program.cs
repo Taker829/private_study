3ca7b310083e80e53506392b3ba597d7c5431a0a
Taker829 <kt829@naver.com>
2020-05-17 17:55:31 +0900

    2020-05-17 study update
    C# class study update

---
+++ 2020/05/study_0517/study_001/study_001/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace study_001
+{
+    class Cat
+    {
+        public string Name;
+        public string Color;
+
+        public void Meow()
+        {
+            WriteLine($"{Name} 고양이가 야옹하고 포효했다.");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.Color = "하얀색";
+            kitty.Name = "키티";
+            kitty.Meow();
+            WriteLine($"{kitty.Name} : {kitty.Color}");
+
+            Cat nero = new Cat();
+            nero.Color = "검은색";
+            nero.Name = "네로";
+            nero.Meow();
+            WriteLine($"{nero.Name} : {nero.Color}");
+        }
+    }
+}
