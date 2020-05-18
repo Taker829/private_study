73302fd32af3f4387a657efe75dcad24162a5d67
Taker829 <kt829@naver.com>
2020-05-18 11:41:49 +0900

    2020-05-18 study update
    C# static field study update

---
+++ 2020/05/study_0518/study_001/study_001/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+// 프로그램 전체에 걸쳐 공유해야 하는 변수 -> 정적 필드 사용
+
+class Global
+{
+    public static int Count = 0;
+}
+
+class ClassA
+{
+    public ClassA()
+    {
+        // Global의 static 변수 Count를 사용
+        Global.Count++;
+    }
+}
+
+class ClassB
+{
+    public ClassB()
+    {
+        Global.Count++;
+    }
+}
+
+namespace study_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine($"Globla.Count_Before : {Global.Count}");
+
+            new ClassA();
+            new ClassA();
+            new ClassB();
+            new ClassB();
+
+            WriteLine($"Globla.Count_After : {Global.Count}");
+        }
+    }
+}
