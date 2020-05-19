eab67801ceb1f6dbecd03366042169818cc68bca
Taker829 <kt829@naver.com>
2020-05-20 07:39:25 +0900

    2020-05-20 study update
    C# this keyword study update

---
+++ 2020/05/study_0520/study_001/study_001/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+
+namespace study_001
+{
+    class Employee
+    {
+        // 객체 내부에서 자신의 변수에 접근 및 사용할 때 this 사용
+        private string Name;
+        private string Position;
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+        
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void setPosition(string Position)
+        {
+            this.Position = Position;
+        }
+
+        public string GetPosition()
+        {
+            return this.Position;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee pooh = new Employee();
+            pooh.SetName("Pooh");
+            pooh.setPosition("Waiter");
+            WriteLine($"{pooh.GetName()} {pooh.GetPosition()}");
+
+            Employee tigger = new Employee();
+            tigger.SetName("Tigger");
+            tigger.setPosition("Cleaner");
+            WriteLine($"{tigger.GetName()} {tigger.GetPosition()}");
+        }
+    }
+}
