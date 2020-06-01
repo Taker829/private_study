c9bdb29f3da8922d22e82de33d0bf7734a509b6b
Taker829 <kt829@naver.com>
2020-06-01 11:03:18 +0900

    2020-06-01 study update
    C# class type casting study update

--- 2020/05/study_0525/study_001/study_001/Program.cs
+++ 2020/05/study_0525/study_001/study_001/Program.cs
@@ -52,3 +52,4 @@ namespace study_001
         }
     }
 }
+  
---
+++ 2020/06/study_0601/study_001/study_001/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using static System.Console;
+
+namespace study_001
+{
+    class Mamal
+    {
+        public void Nurse()
+        {
+            WriteLine("Nurse()");
+        }
+    }
+    class Dog : Mamal
+    {
+        public void Bark()
+        {
+            WriteLine("Bark()");
+        }
+    }
+    class Cat : Mamal
+    {
+        public void Meow()
+        {
+            WriteLine("Meow()");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mamal mamal = new Dog();
+            Dog dog;
+
+            if (mamal is Dog)
+            {
+                dog = (Dog)mamal;
+                dog.Bark();
+            }
+
+            Mamal mamal2 = new Cat();
+
+            Cat cat = mamal2 as Cat;
+            if (cat != null)
+                cat.Meow();
+
+            Cat cat2 = mamal as Cat;
+            if (cat2 != null)
+                cat2.Meow();
+            else
+                WriteLine("cat2 is not a Cat");
+        }
+    }
+}
